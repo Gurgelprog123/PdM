08356a46ff7fb9471af92a9f2221ded8e1c856bc
Gurgelprog123 <jp.gurgel.17@gmail.com>
2025-04-11 22:09:02 -0300

    Atividade Pratica

--- App/labels.js
+++
@@ -1,7 +0,0 @@
-export const labels = {
-    appTitle: 'Cadastro de Usuário',
-    nameLabel: 'Nome:',
-    phoneLabel: 'Telefone:',
-    registerButton: 'Cadastrar',
-    registeredUsersTitle: 'Usuários Cadastrados'
-  };
